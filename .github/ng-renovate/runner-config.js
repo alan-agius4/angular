bf0d26e07695909a71e42653ac902925389ee667
Alan Agius <alanagius@google.com>
2024-11-02 14:45:23 +0000

    asd

--- .github/ng-renovate/runner-config.js
+++ .github/ng-renovate/runner-config.js
@@ -6,6 +6,7 @@ module.exports = {
   // not be able to delete the branches and future updates would be missed.
   forkModeDisallowMaintainerEdits: true,
   onboarding: false,
+  forkProcessing: 'enabled',
   persistRepoData: true,
   allowedPostUpgradeCommands: ['.', '^yarn install$', '^yarn update-generated-files$'],
   repositories: ['alan-agius4/angular'],
